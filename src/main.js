b9cfaccf6b82a826db8dc21c538a7d85c70a210e
kwstn <alex_costi@icloud.com>
2023-11-21 19:51:06 +0000

    hero profile

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import '../styles/modern-normalise.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
+import '../styles/components/hero.css';
 import '../styles/utils.css';
